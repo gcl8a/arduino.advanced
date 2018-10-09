a375640344d11d14abdb49e168bd6c2d6d59490c
GCL <lewin.gc@gmail.com>
2018-10-09 19:50:41 -0400

    cleaned out solutions

--- alarm_skeleton/alarm_skeleton.ino
+++ alarm_skeleton/alarm_skeleton.ino
@@ -4,18 +4,22 @@
 Adafruit_PN532 nfc(2, 3);
 
 //This will be the ID of the tag you will use to deactivate your system
-//You'll have to edit these for your specific tag
-uint8_t targetID[] = {0x7B, 0x46, 0x20, 0x2D};
+//YOU'LL HAVE TO EDIT THESE FOR YOUR SPECIFIC TAG!!
+byte targetID[] = {0x7B, 0x46, 0x20, 0x2D};
 
-enum ALARM_STATE {ALARM_OFF, ALARM_ARMED, ALARM_INTRUDER};
-ALARM_STATE currentState = ALARM_OFF;
+//define the states in easy-to-read terms
+enum {ALARM_OFF, ALARM_ARMED, ALARM_INTRUDER};
+int currentState = ALARM_OFF; //start off with the alarm deactivated
 
+//Some pin definitions. The RFID is taken care of for us.
 const int buttonPin = 7;
-const int ledPin = 13;
+const int photoPin  = A0;
+const int piezoPin  = 11;
+const int ledPin    = 13;
 
 void setup(void) 
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("Hello!");
 
   if(!SetupRFIDShield()) //sets up the RFID
@@ -24,13 +28,18 @@ void setup(void)
     while(1) {}
   }
 
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT_PULLUP); //if we use INPUT_PULLUP, we don't have to have an external pullup resistor
+  pinMode(photoPin, INPUT);
+  pinMode(piezoPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
 
   Serial.println("setup() complete");
 }
 
 void loop(void)
+/*
+ * Our loop() is just a set of checker-handler pairs.
+ */
 {
   if(CheckArmingButton()) HandleArmingButton();
   if(CheckIfLaserBroken()) HandleLaserBroken();
@@ -39,50 +48,93 @@ void loop(void)
 
 void HandleArmingButton(void)
 {
-  //here we implement our state machine
-  switch(currentState)
+  if(currentState == ALARM_OFF)
   {
-    case ALARM_OFF:
-      //you'll have to put in the code to arm
-      Serial.println("Arming!");
-      digitalWrite(ledPin, HIGH);
-      currentState = ALARM_ARMED;
-      break;
-    //nothing happens for the other cases -- pushing the button has no effect
-    default:
-      break;
+    Serial.println("Arming!");
+
+    //take the action: light the LED
+    digitalWrite(ledPin, HIGH);    
+    
+    currentState = ALARM_ARMED; //Don't forget to change the state!
   }
 }
 
 void HandleLaserBroken(void)
 {
-  //here we implement our state machine
-  switch(currentState)
-  {
-    case ALARM_ARMED:
-      //add code to sound the alarm;
-      break;
-    default: 
-      //nothing happens for any of the other states
-      break;
-  }
+  //YOUR CODE HERE!
 }
 
 void HandleRFID(void)
 {
-  //here we implement our state machine
-  switch(currentState)
+  //YOUR CODE HERE!
+}
+
+/*
+ * Code for checking the button
+ */
+int prevButtonState = HIGH; //button up => pin reads HIGH
+
+bool CheckArmingButton(void)
+{
+  bool retVal = false;
+  
+  int currButtonState = digitalRead(buttonPin);
+  if(prevButtonState != currButtonState)
   {
-    case ALARM_ARMED:
-    case ALARM_INTRUDER:
-      Serial.println("Correct tag detected. Disarming.");
-      digitalWrite(ledPin, LOW);
-      currentState = ALARM_OFF;
-      //you're job to code here
-      break;
-    default:
-      break;
+    delay(10); //this is a cheat for debouncing
+    if(currButtonState == LOW) retVal = true;  //button is down => pin reads LOW
   }
+  prevButtonState = currButtonState;
+
+  return retVal;
+}
+
+/*
+ * Code for checking the laser.
+ */
+enum {DARK, LIGHT};
+int prevLaserSensorState = DARK; //the program starts with the laser off
+
+bool CheckIfLaserBroken(void)
+{
+  bool retVal = false;
+
+  //add code here to detect the EVENT of the laser being broken. See the button checker above for hints
+  
+  return retVal;
+}
+
+
+bool CheckForRFID(void) 
+/*
+ * The RFID is complicated enough that we've taken care of it for you. Note, however, that this checker
+ * isn't true event-driven -- it will send success repeatedly if you hold a tag near it. Luckily, because
+ * we've implememted a state machine, all of the extra successes will be ignored.
+ */
+{
+  byte uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  byte uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+    
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  bool success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 40);
+    
+  if (success) 
+  {
+    Serial.println("Tag found!");
+    if(uidLength == 4)
+    {
+      Serial.print(uid[0]);//...
+      if(targetID[0] == uid[0] && targetID[1] == uid[1] && targetID[2] == uid[2] && targetID[3] == uid[3])
+      {
+        Serial.println("Match!");
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 bool SetupRFIDShield(void)
@@ -100,54 +152,5 @@ bool SetupRFIDShield(void)
   return true;
 }
 
-int prevButtonState = 1;
-bool CheckArmingButton(void)
-{
-  bool retVal = false;
-  
-  int currButtonState = digitalRead(buttonPin);
-  if(prevButtonState != currButtonState)
-  {
-    delay(10);
-    if(currButtonState == 0) retVal = true;
-  }
-  prevButtonState = currButtonState;
 
-  return retVal;
-}
-
-bool CheckForRFID(void) 
-{
-  uint8_t success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-    
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 20);
-    
-  if (success) 
-  {
-    Serial.println("Tag found!");
-    if(uidLength == 4)
-    {
-      if(targetID[0] == uid[0] && targetID[1] == uid[1] && targetID[2] == uid[2] && targetID[3] == uid[3])
-      {
-        return true;
-      }
-    }
-  }
-
-  return false;
-}
-
-bool CheckIfLaserBroken(void)
-{
-  bool returnValue = false;
-
-  //add code here...
-  
-  return returnValue;
-}
 
